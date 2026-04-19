c0e66e9b8f6ccf22e2470941bba09ff89e79780e
Hugh Cole-Baker <sigmaris@gmail.com>
2026-04-19 10:49:37 +0100

    Include qcom firmware in initramfs

---
+++ overlays/radxa-qcs6490-firmware/etc/initramfs-tools/hooks/include-firmware.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+PREREQ=""
+prereqs() { echo "$PREREQ"; }
+case $1 in
+    prereqs) prereqs; exit 0;;
+esac
+. /usr/share/initramfs-tools/hook-functions
+# Copy required firmware
+mkdir -p "${DESTDIR}/lib/firmware/qcom/qcs6490/radxa"
+cp -r /lib/firmware/qcom/qcs6490/radxa/dragon-q6a "${DESTDIR}/lib/firmware/qcom/qcs6490/radxa/dragon-q6a"
+cp /lib/firmware/qcom/a660_sqe.fw "${DESTDIR}/lib/firmware/qcom/a660_sqe.fw"
