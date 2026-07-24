5217146af40bb2a201c28e7613eaf77948d04257
Hugh Cole-Baker <sigmaris@gmail.com>
2026-07-24 22:25:05 +0100

    use radxa trixie repos
    Untested so far

--- overlays/radxa-qcs6490-firmware/etc/initramfs-tools/hooks/include-firmware.sh
+++ overlays/radxa-qcs6490-firmware/etc/initramfs-tools/hooks/include-firmware.sh
@@ -8,4 +8,5 @@ esac
 # Copy required firmware
 mkdir -p "${DESTDIR}/lib/firmware/qcom/qcs6490/radxa"
 cp -r /lib/firmware/qcom/qcs6490/radxa/dragon-q6a "${DESTDIR}/lib/firmware/qcom/qcs6490/radxa/dragon-q6a"
-cp /lib/firmware/qcom/a660_sqe.fw "${DESTDIR}/lib/firmware/qcom/a660_sqe.fw"
+cp /lib/firmware/qcom/a660_sqe.fw /lib/firmware/qcom/a660_gmu.bin "${DESTDIR}/lib/firmware/qcom/"
+cp /lib/firmware/qcom/qcs6490/a660_zap.mbn "${DESTDIR}/lib/firmware/qcom/qcs6490/"
